1db718d1a0745d689256d9d9874d7853f9e2131e
Rahat333 <rahat.ahsan13@gmail.com>
2024-09-07 01:00:18 +0600

    Content model added

---
+++ Backend/SchoolSync/app/Models/Content.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Content extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'course_id',
+        'name',
+        'pdf',
+        'image',
+        'short_description',
+        'long_description',
+    ];
+}
